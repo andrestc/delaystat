7d6d89e946a779e4ad37f7bbd2f5ec55d8017b99
André Carvalho <asantostc@gmail.com>
2018-08-20 10:33:10 -0300

    simplify output

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"text/tabwriter"
 
 	"github.com/mdlayher/taskstats"
 )
@@ -26,16 +28,40 @@ func main() {
 
 	var stats *taskstats.Stats
 	if *pid != -1 {
+		fmt.Printf("pid %v\n", pid)
 		stats, err = client.PID(*pid)
 	} else {
+		fmt.Printf("tgid %v\n", *tgid)
 		stats, err = client.TGID(*tgid)
 	}
 	if err != nil {
 		log.Panic(err)
 	}
 
-	err = PrintStats(stats)
-	if err != nil {
+	if err := PrintStats(stats, nil); err != nil {
 		log.Panic(err)
 	}
+}
+
+func PrintStats(stats *taskstats.Stats, lastStats *taskstats.Stats) error {
+	diffStats := *stats
+	if lastStats != nil {
+		diffStats.CPUDelay -= lastStats.CPUDelay
+		diffStats.CPUDelayCount -= lastStats.CPUDelayCount
+		diffStats.BlockIODelay -= lastStats.BlockIODelay
+		diffStats.BlockIODelayCount -= lastStats.BlockIODelayCount
+		diffStats.FreePagesDelay -= lastStats.FreePagesDelay
+		diffStats.FreePagesDelayCount -= lastStats.FreePagesDelayCount
+		diffStats.SwapInDelay -= lastStats.SwapInDelay
+		diffStats.SwapInDelayCount -= lastStats.SwapInDelayCount
+	}
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', tabwriter.TabIndent)
+	fmt.Fprintln(w, "CPU\tI/O\tSwap\tMemory Reclaim")
+	fmt.Fprintf(w, "%v\t%v\t%v\t%v\n",
+		diffStats.CPUDelay,
+		diffStats.BlockIODelay,
+		diffStats.SwapInDelay,
+		diffStats.FreePagesDelay,
+	)
+	return w.Flush()
 }
